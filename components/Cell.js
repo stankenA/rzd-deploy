2d0db63fd9807915267104891d547c050bee4489
stanken_a <artyemst3@mail.ru>
2023-02-16 18:49:10 +0300

    dam2

--- components/Cell.js
+++ components/Cell.js
@@ -19,7 +19,7 @@ export default class Cell {
   }
 
   _checkClientWidth() {
-    if (document.documentElement.clientWidth < 1024) {
+    if (document.documentElement.clientWidth < 768) {
       this.close();
     }
   }
